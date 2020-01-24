9964e4180c4bedb9247dff327557b613823830ab
Luciano <luciano.bestia@gmail.com>
2020-01-24 18:05:49 +0700

    eerror handling anyhow

--- src/main.rs
+++ src/main.rs
@@ -27,13 +27,13 @@
 
 //region: use statements
 use ansi_term::Colour::{Green, Red, Yellow};
+use anyhow::{anyhow, Context};
 use clap::{App, Arg};
 use dirs;
 use regex::Regex;
 use std::env;
 use std::fs;
 use std::path::Path;
-use unwrap::unwrap;
 //endregion
 
 /// The CLI program starts here.  
@@ -61,29 +61,43 @@ fn main() {
         );
 
         let versions = get_versions(&crate_name);
-        println!(
-            "{} '{}' {:#?}",
-            Red.paint("Versions of crate"),
-            Red.paint(crate_name),
-            versions
-        );
+        match versions {
+            Ok(versions) => {
+                println!(
+                    "{} '{}' {:#?}",
+                    Green.paint("Versions of crate"),
+                    Green.paint(crate_name),
+                    versions
+                );
+            }
+            Err(err) => {
+                println!("Error: '{}' {}", Red.paint(crate_name), err);
+            }
+        }
 
         let found_names = search_crates(crate_name);
-        println!(
-            "{} '{}' {:#?}",
-            Green.paint("Found crate names that contain"),
-            Green.paint(crate_name),
-            found_names
-        );
+        match found_names {
+            Ok(found_names) => {
+                println!(
+                    "{} '{}' {:#?}",
+                    Green.paint("Found crate names that contain"),
+                    Green.paint(crate_name),
+                    found_names
+                );
+            }
+            Err(err) => {
+                println!("Error: '{}' {}", Red.paint(crate_name), err);
+            }
+        }
     }
 }
 
 /// Get versions from the local cache of the crates.io index.  
-pub fn get_versions(crate_name: &str) -> Vec<String> {
+pub fn get_versions(crate_name: &str) -> anyhow::Result<Vec<String>> {
     let mut versions = Vec::new();
     // the linux shell home dir symbol ~ or HOME is not expanded in raw rust
     // I must use the dirs crate
-    let mut folder = unwrap!(dirs::home_dir());
+    let mut folder = dirs::home_dir().context("Not home dir")?;
     folder.push(".cargo/registry/index/github.com-1ecc6299db9ec823/.cache");
     //interesting rules for the folder structure
     if crate_name.len() == 1 {
@@ -99,15 +113,19 @@ pub fn get_versions(crate_name: &str) -> Vec<String> {
     //println!("Folder: {:?}", &folder);
     let dir = Path::new(&folder);
     if dir.exists() {
-        for entry in unwrap!(fs::read_dir(dir)) {
+        for entry in fs::read_dir(dir)? {
             //crazy amount of unwrap to go from path to a normal string
-            let path = unwrap!(entry).path();
-            let entry_file_name = unwrap!(unwrap!(path.file_name()).to_str());
+            let path = entry?.path();
+            let entry_file_name = path
+                .file_name()
+                .context("error file name not exists")?
+                .to_str()
+                .context("error file name to str")?;
             if entry_file_name == crate_name {
                 //read the content and find versions
-                let file_content = unwrap!(fs::read_to_string(path));
+                let file_content = fs::read_to_string(path)?;
                 //I will use regex to find all "vers": "0.3.3",
-                let re = unwrap!(Regex::new(r#""vers":"(.*?)".*?"yanked":(.*?)[,}]"#));
+                let re = Regex::new(r#""vers":"(.*?)".*?"yanked":(.*?)[,}]"#)?;
                 for cap in re.captures_iter(&file_content) {
                     //println!("version: {} yanked: {}", &cap[1], &cap[2]);
                     if &cap[2] == "false" {
@@ -118,21 +136,29 @@ pub fn get_versions(crate_name: &str) -> Vec<String> {
             }
         }
     }
-    //I need the latest first
-    versions.reverse();
-    //return
-    versions
+    if versions.is_empty() {
+        return Err(anyhow!("crate not found"));
+    } else {
+        //I need the latest first
+        versions.reverse();
+        //return
+        Ok(versions)
+    }
 }
 
 /// Search local crates.io index cache for a crate name that contains a substring.  
-pub fn search_crates(crate_name_substring: &str) -> Vec<(String, String)> {
-    let mut folder = unwrap!(dirs::home_dir());
+pub fn search_crates(crate_name_substring: &str) -> anyhow::Result<Vec<(String, String)>> {
+    let mut folder = dirs::home_dir().context("no home dir")?;
     folder.push(".cargo/registry/index/github.com-1ecc6299db9ec823/.cache");
     let dir = Path::new(&folder);
     let mut found_names: Vec<(String, String)> = Vec::new();
-    search_file_name_recursive(&dir, &crate_name_substring, &mut found_names);
+    search_file_name_recursive(&dir, &crate_name_substring, &mut found_names)?;
     //return
-    found_names
+    if found_names.is_empty() {
+        return Err(anyhow!("crate substring not found"));
+    } else {
+        Ok(found_names)
+    }
 }
 
 /// Search for files_names containing a substring, recursive.  
@@ -140,25 +166,30 @@ fn search_file_name_recursive(
     dir: &Path,
     file_name_substring: &str,
     found_names: &mut Vec<(String, String)>,
-) {
+) -> anyhow::Result<()> {
     if dir.is_dir() {
         //println!("This is dir: {:?}", dir.file_name());
-        for entry in fs::read_dir(dir).unwrap() {
-            let entry = entry.unwrap();
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
             let path = entry.path();
-            let file_name = path.file_name().unwrap().to_str().unwrap();
+            let file_name = path
+                .file_name()
+                .context("no file name improbable")?
+                .to_str()
+                .context("no to_str improbable")?;
             if path.is_dir() {
-                search_file_name_recursive(&path, file_name_substring, found_names);
+                search_file_name_recursive(&path, file_name_substring, found_names)?;
             } else if file_name.contains(file_name_substring) {
                 //println!("Found it: {:?}", path);
                 //there is no description in this files, only versions and deps
-                let v = get_versions(file_name);
+                let v = get_versions(file_name).context("no get version")?;
                 //i need only the last version
-                let last_version = unwrap!(v.first());
+                let last_version = v.first().context("no first vector")?;
                 found_names.push((file_name.to_string(), last_version.to_string()));
             } else {
                 //nothing
             }
         }
     }
+    return Ok(());
 }
